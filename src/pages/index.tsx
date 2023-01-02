ad052b33075326e9ee78fb9b76332d2fb9446580
feliippeee <felipe.tecnosuporte@gmail.com>
2023-01-02 03:23:00 -0300

    Setup & Estrutura: Instalação do Design System

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { Heading } from '@ignite-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>hello word</h1>  
+    <Heading>hello word</Heading>  
   )
 }
