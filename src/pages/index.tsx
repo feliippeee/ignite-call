e775017e59f1802de08c403af2a7cff4b46960b2
feliippeee <felipe.tecnosuporte@gmail.com>
2023-01-02 18:37:54 -0300

    Setup & Estrutura: Configuração do ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import { Heading } from '@ignite-ui/react'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-  return (
-    <Heading>hello word</Heading>  
-  )
+  return <Heading>Hello Word</Heading>
 }
