79d7a93800dba3bc719eddda1642ef0f5f01a28c
feliippeee <felipe.tecnosuporte@gmail.com>
2023-01-07 07:09:00 -0300

    Cadastro de usuário: Manipulando cookies no Next

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -45,8 +46,12 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+       return alert(err.response.data.message)
+      }
+
+      console.error(err)
     }
   }
 
