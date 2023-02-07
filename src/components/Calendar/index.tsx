b65f9ce3b4ce85c95a48bed3fa0068ad6449f8d1
feliippeee <felipe.tecnosuporte@gmail.com>
2023-02-06 23:45:16 -0300

    Calendário e disponibilidade - Finalizando query de dias lotados

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: Number[]
 }
 
 interface CalendarProps {
@@ -59,15 +60,17 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const username = String(router.query.username)
 
   const { data: blockedDates } = useQuery<BlockedDates>(
-    ['blocked-dates', currentDate.get('year'), currentDate.get('month')],
+    ['blocked-dates', 
+      currentDate.get('year'),  
+      currentDate.get('month')],
     async () => {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: String(currentDate.get('month') + 1).padStart(2, '0')
         },
       })
-
+      console.log('DateString', response.data)
       return response.data
     },
   )
@@ -114,7 +117,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
